ea10d68cf0d4ed4b8cd820150d92f31c24fddf10
Chris Trujillo <ctrujillo@esignsystems.com>
2015-11-25 22:22:53 -0700

    init checkin

---
+++ WebHook/Controllers/WebHookController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json;
+using WebHook.Models;
+
+namespace WebHook.Controllers
+{
+    public class WebHookController : ApiController
+    {
+
+        public string Get()
+        {
+            return "send events here";
+        }
+
+
+        [HttpPost]
+        public async Task<Message> Post()
+        {
+            string contentBody;
+            var message = new Message();
+
+            contentBody = await Request.Content.ReadAsStringAsync();
+            message.ClientAddress = HttpContext.Current.Request.UserHostAddress;
+            message.ContentBody = contentBody;
+            ProgressHub.SendMessage(JsonConvert.SerializeObject(message));
+
+            return message;
+        }
+    }
+}
---
+++ WebHook/Hub/ProgressHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebHook
+{
+    public class ProgressHub : Hub
+    {
+
+        public static void SendMessage(string msg)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<ProgressHub>();
+            hubContext.Clients.All.sendMessage(msg);
+        }
+    }
+}
